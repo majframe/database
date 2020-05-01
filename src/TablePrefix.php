c97a85ac0581e6b43265f6a73bc794c7ce88196f
Ondřej Maxa <ondrej.maxa.ih1@gmail.com>
2020-05-02 01:10:23 +0200

    EntityManagerFactory.php and TablePrefix.php.

---
+++ src/TablePrefix.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * 2020-2020 Majframe
+ *
+ *  NOTICE OF LICENSE
+ *
+ * This source file is subject to the GNU General Public License (GPL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @copyright 2020-2020 Majframe
+ * @license   https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License (GPL 3.0)
+ */
+
+namespace Majframe\Database;
+
+
+use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
+
+
+/**
+ * Class TablePrefix
+ * @package Majframe\Database
+ */
+class TablePrefix
+{
+    /**
+     * @var string $prefix
+     */
+    protected string $prefix = '';
+
+    /**
+     * TablePrefix constructor.
+     * @param string $prefix
+     */
+    public function __construct(string $prefix)
+    {
+        $this->prefix = $prefix;
+    }
+
+    /**
+     * @param LoadClassMetadataEventArgs $eventArgs
+     */
+    public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs): void
+    {
+        $classMetadata = $eventArgs->getClassMetadata();
+
+        if (!$classMetadata->isInheritanceTypeSingleTable() || $classMetadata->getName() === $classMetadata->rootEntityName) {
+            $classMetadata->setPrimaryTable([
+                'name' => $this->prefix . $classMetadata->getTableName()
+            ]);
+        }
+
+        foreach ($classMetadata->getAssociationMappings() as $fieldName => $mapping) {
+            if ($mapping['type'] === ClassMetadataInfo::MANY_TO_MANY && $mapping['isOwningSide']) {
+                $mappedTableName = $mapping['joinTable']['name'];
+                $classMetadata->associationMappings[$fieldName]['joinTable']['name'] = $this->prefix . $mappedTableName;
+            }
+        }
+    }
+}
